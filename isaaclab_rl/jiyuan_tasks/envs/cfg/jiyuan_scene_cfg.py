8ef7ed3dc510c8e0eb5d03fa360ddd9b0e19d103
chenpeel <chgenpeel@foxmail.com>
2025-12-29 15:48:29 +0800

    fix: termination joints

--- isaaclab_rl/jiyuan_tasks/envs/cfg/jiyuan_scene_cfg.py
+++ isaaclab_rl/jiyuan_tasks/envs/cfg/jiyuan_scene_cfg.py
@@ -165,8 +165,8 @@ class JiyuanSceneCfg(InteractiveSceneCfg):
             ),
             articulation_props=sim_utils.ArticulationRootPropertiesCfg(
                 enabled_self_collisions=False,
-                solver_position_iteration_count=4,
-                solver_velocity_iteration_count=0,
+                solver_position_iteration_count=16,
+                solver_velocity_iteration_count=1,
             ),
         ),
     )
